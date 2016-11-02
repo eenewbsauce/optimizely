e72b4b8d718fa798c58a653b75d0cdd345e91ca0
eenewbsauce <ryan.m.tracey@gmail.com>
2016-11-02 16:09:01 -0400

    refactor(auth): use variable for expiry

--- index.js
+++ index.js
@@ -2,37 +2,7 @@ const optlyLib    = require('./lib');
 let optly;
 
 optlyLib.create()
-    .then(optlyInstance => {
-        optly = optlyInstance;
-    })
+    .then(optlyInstance => {optly = optlyInstance})
     .then(() => optly.projects.list({ name: 'RocketLoans Dev' }))
     .then(projectId => optly.campaigns.list(projectId))
     .then(campaigns => optly.campaigns.duplicate(campaigns[0]));
-
-
-
-
-
-    
-
-// var express = require('express');
-// var app = express();
-
-// app.get('/', function (req, res) {
-//     auth.accessToken()
-//         // .then(() => {
-//         //     console.log(JSON.stringify(optlyLib, null, 4));
-//         //     projects    = new optlyLib.projects();
-//         //     experiments = new optlyLib.experiments();
-//         // })
-//         // .catch(() => auth.refreshToken())
-//         // .then(() => projects.list({ name: 'RocketLoans DEV'}))
-//         // .then(projectId => experiments.list(projectId))
-//         // .then(response => {
-//         //     console.log(JSON.stringify(response, null, 4));
-//         // });
-// });
-//
-// app.listen(8080, function () {
-//     console.log('Example app listening on port 8080!');
-// });
