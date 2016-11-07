75e708fb0fd4871ff39898eea97a8abf04e7d690
eenewbsauce <ryan.m.tracey@gmail.com>
2016-11-07 09:50:09 -0500

    feat(pages): fetch page by id working

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const colors      = require('colors');
+
 const optlyLib    = require('./lib');
 let optly;
 
@@ -5,4 +7,5 @@ optlyLib.create()
     .then(optlyInstance => {optly = optlyInstance})
     .then(() => optly.projects.list({ name: 'RocketLoans Dev' }))
     .then(projectId => optly.campaigns.list(projectId))
-    .then(campaigns => optly.campaigns.duplicate(campaigns[0]));
+    .then(campaigns => optly.campaigns.duplicate(campaigns[0]))
+    .catch(err => consolel.log(err).red);
